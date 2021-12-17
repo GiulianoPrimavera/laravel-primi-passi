b69a3fb7e741bd39dbd942b22d6f895397e085c6
GiulianoPrimavera <giuliano.plrimavera@gmail.com>
2021-12-17 15:45:34 +0100

    makes the pages on the lista pagine as objects containing name and link for the page

--- routes/web.php
+++ routes/web.php
@@ -17,9 +17,18 @@ Route::get('/', function () {
     $data = [
         'hello' => 'hello world',
         'lista_pagine' => [
-            'pagina_1',
-            'pagina_2',
-            'pagina_3',
+            'pagina_1'=> [
+                'nome_pagina' => 'pagina_1',
+                'link_pagina' => "pagina_1"
+            ],
+            'pagina_2'=> [
+                'nome_pagina' => 'pagina_2',
+                'link_pagina' => "pagina_2"
+            ],
+            'pagina_3'=> [
+                'nome_pagina' => 'pagina_3',
+                'link_pagina' => "pagina_3"
+            ],
         ]
     ];
     return view('home', $data);
@@ -29,6 +38,21 @@ Route::get('/pagina_1', function(){
     $data = [
         'titolo_pagina' => 'sono la pagina 1'
     ];
-
     return view('pagina_1', $data);
 })->name('pagina_1');
+
+
+Route::get('/pagina_2', function(){
+    $data = [
+        'titolo_pagina' => 'sono la pagina 2'
+    ];
+    return view('pagina_2', $data);
+})->name('pagina_2');
+
+
+Route::get('/pagina_3', function(){
+    $data = [
+        'titolo_pagina' => 'sono la pagina 3'
+    ];
+    return view('pagina_3', $data);
+})->name('pagina_3');
