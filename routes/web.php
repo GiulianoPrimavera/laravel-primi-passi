ce9ef91ed9f58b0c95d2c5870509a866e20a7e3a
GiulianoPrimavera <giuliano.plrimavera@gmail.com>
2021-12-17 15:20:48 +0100

    adds data to the route

--- routes/web.php
+++ routes/web.php
@@ -14,5 +14,13 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('welcome');
+    $data = [
+        'hello' => 'hello world',
+        'lista_pagine' => [
+            'pagina_1',
+            'pagina_2',
+            'pagina_3',
+        ]
+    ];
+    return view('home', $data);
 });
